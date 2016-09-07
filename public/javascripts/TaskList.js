e7518244af19715145e23e84f0460bde72ae9eaf
BrandonEchols <bechols@digis.net>
2016-09-07 15:48:34 -0600

    Initial commit

---
+++ public/javascripts/TaskList.js
@@ -0,0 +1,119 @@
+/**
+ * Created by Brandon on 8/28/2016.
+ */
+(function (global) {
+    var TaskList = function TaskList() {
+        var initialTestRun = true;
+        var taskModel = new TaskModel();
+        var that;
+        var taskTableElementId;
+        TaskList.prototype.newList = function newList(elementId) {
+            that = this;
+            taskTableElementId = elementId;
+            //Remove old Content
+            document.getElementById(elementId).innerHTML = "";
+
+            initTaskList(elementId);
+        }
+
+        TaskList.prototype.updateDisplay = function updateDisplay() {
+            initTaskList("TaskList");
+            if (taskModel.getListLength() > 0) {
+                for (var i = 0; i < taskModel.getListLength(); i++) {
+                    taskModel.displayTask(i);
+                }
+            }
+        }
+
+        TaskList.prototype.taskInit = function taskInit(name) {
+            var name = document.getElementById("inputBox").value;
+            if (name) {
+                taskModel.initTask(name);
+            }
+        }
+
+        /*taskTable.prototype.restoreList = function restoreList(elementId, gameState) {
+         //Remove old Content
+         document.getElementById(elementId).innerHTML = "";
+
+         //Create a personalized greeting and display it in the welcome sign
+         var greeting = "Welcome back to Trying Tasks " + gameState.name + "! :-)"
+         document.getElementById("welcomeSign").innerHTML = greeting;
+
+         //Create new game using parameters
+         initTaskList(elementId, gameState.list);
+
+         }*/
+
+        /*taskTable.prototype.openTaskListFile = function openTaskListFile(files) {
+         var fileReader = new FileReader();
+
+         //Add a listener to the fileReader to wait until it is done reading
+         fileReader.addEventListener("load", function(event) {
+         var savedGame = JSON.parse(fileReader.result);
+         that.restoreList(taskTableElementId, savedGame);
+         })
+         fileReader.readAsText(files[0]);
+         }*/
+
+        /** Constructor
+         * */
+        function initTaskList(elementId) {
+            function initList(elementId) {
+                var table = document.getElementById(elementId);
+                table.innerHTML = "";
+                var tableRow = document.createElement("TR");
+                tableRow.className += " sign taskListMenu";
+
+                var col0 = document.createElement("TH");
+                col0.innerHTML = "#";
+                tableRow.appendChild(col0);
+
+                var col1 = document.createElement("TH");
+                col1.innerHTML = "Task";
+                tableRow.appendChild(col1);
+
+                var col2 = document.createElement("TH");
+                col2.innerHTML = "Created";
+                tableRow.appendChild(col2);
+
+                var col3 = document.createElement("TH");
+                col3.innerHTML = "Completed?";
+                tableRow.appendChild(col3);
+
+                table.appendChild(tableRow);
+
+            }
+
+            initList(elementId);
+
+            //TODO: THIS IS FOR DUBUGGING PURPOSES TEMPORARILY
+            if (initialTestRun) {
+                initialTestRun = false;
+                tempTaskInit();
+            }
+            function tempTaskInit() {
+                taskModel.initTask("Walk the dog");
+                taskModel.initTask("Tell my wife I love her!");
+                taskModel.initTask("Go sit outside");
+                taskModel.initTask("Do the Dishes!!!!!!");
+            }
+
+            //^^^^^^^^^^^^^^^^
+        }
+
+        return this;
+
+
+    };
+
+    /**
+     * export for CommonJS style
+     */
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = new TaskList;
+    } else {
+        global.TaskList = TaskList;
+    }
+
+}(this));
---
+++ public/javascripts/TaskModel.js
@@ -0,0 +1,130 @@
+/**
+ * Created by Brandon on 8/28/2016.
+ */
+(function(global) {
+
+    /** Constructor */
+    var TaskModel = function TaskModel(){
+
+        var model = {
+            list : []
+        }
+
+        //Public functions
+        this.getListLength = function getListLength(){
+            return model.list.length;
+        }
+        this.isComplete = function isComplete(taskIdx)
+        {
+            if(taskIdx < model.list.length) {
+                var queryComplete = model.list[taskIdx].isComplete;
+                return queryComplete;
+            }
+            return false;
+        }
+
+        this.initTask = function initTask(name){
+            var task = {
+                name: name ? name : untitled, //This is the default name, untitled
+                timeMade : "TBD", //TODO: timeMade = current time stamp
+                isComplete : false
+            }
+            model.list[model.list.length] = task;
+            this.displayTask(model.list.length - 1);
+        }
+
+        this.displayTask = function displayTask(i){
+            var table = document.getElementById("TaskList");
+
+            var row = document.createElement("TR");
+            row.className = "task";
+            var col = document.createElement("TD");
+            col.innerHTML = "" + (i + 1);
+            row.appendChild(col);
+
+            var col = document.createElement("TD");
+            col.innerHTML = "" + model.list[i].name;
+            col.className += " taskName";
+            row.appendChild(col);
+
+            var col = document.createElement("TD");
+            col.innerHTML = "" + model.list[i].timeMade;
+            row.appendChild(col);
+
+            var col = document.createElement("TD");
+            if(model.list[i].isComplete) {
+                col.innerHTML = "&#10003";
+            }
+            else{
+                col.innerHTML = "&#10007";
+            }
+            row.appendChild(col);
+
+            //Create a button that is either a complete button if the task
+            //is not complete, or a remove button if it is complete
+            var btn = document.createElement("BUTTON");
+            btn.className += " button";
+            if (model.list[i].isComplete == true)
+            {
+                var t = document.createTextNode("Remove");       // Create a text node
+                btn.appendChild(t);
+                btn.onclick = function () {
+                    for (var j = i; j < model.list.length - 1; j++){
+                        model.list[j] = model.list[j + 1];
+                    }
+                    model.list.splice(model.list.length - 1);
+                    tl.updateDisplay();
+                }
+            }
+            else {
+                var t = document.createTextNode("Complete");       // Create a text node
+                btn.appendChild(t);
+                btn.onclick = function () {
+                    model.list[i].isComplete = true;
+                    tl.updateDisplay();
+                }
+            }
+            row.appendChild(btn);
+            table.appendChild(row);
+        }
+
+        //Private functions
+        /*function initGrid(){
+
+         var tiles = [];
+         for(var i = 0; i < model.height * model.width; i++)
+         {
+         tiles.push(i);
+         }
+         shuffle(tiles);
+
+         model.rows = [];
+         for (var rowIdx = 0; rowIdx < model.height; rowIdx++){
+         var row = [];
+         for (var colIdx = 0; colIdx < model.width; colIdx++)
+         {
+         var cell = tiles[(rowIdx * model.width) + colIdx];
+         row.push(cell);
+         }
+         model.rows.push(row);
+         }
+         }*/
+
+        function init(){
+            //TODO: If a saved task list is present, init saved list
+        }
+
+        init();
+        return this;
+    }
+
+    /**
+     * export for CommonJS style
+     */
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = TaskModel;
+    } else {
+        global.TaskModel = TaskModel;
+    }
+
+}(this));
